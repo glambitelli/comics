aebde50e8417ad380fcc5e3a93b40689a4d1e177
glambitelli <127194216+glambitelli@users.noreply.github.com>
2026-06-11 10:00:47 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,20 +1,4 @@
-const CACHE = 'fumetto-v1';
-const ASSETS = ['./index.html', './manifest.json'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request))
-  );
-});
+// Service worker minimal - no caching to avoid blocking Firebase
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', () => self.clients.claim());
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
